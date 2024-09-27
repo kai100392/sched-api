2aee031308e5470350152926f0c048c55792991b
Yaxiong Lin <lin.yaxiong@mayo.edu>
2024-09-27 09:11:51 -0400

    updated test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
 
-curl -X POST "http://localhost:8000/patient-like-me" -d @test.json
+#curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://dev.cdh-az-sched-n.caf.mccapp.com/patient-like-me" -d @test.json 
 
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" "http://localhost:8000/patient-like-me" -d @test.json 
