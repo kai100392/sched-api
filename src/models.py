d1f91d8ce89a369a3cf68ed6d8f113bb9dd8b5e2
Lin, Yaxiong <Lin.Yaxiong@mayo.edu>
2024-10-08 13:27:08 +0000

    Updated models.py

--- src/models.py
+++ src/models.py
@@ -6,3 +6,14 @@ class ScheduleResponse (BaseModel):
     scheduling_actions: []
     similar_patients: []
 
+class SimilarPatient (BaseModel):
+    clinic_number: int
+    callin_date: str
+    appt_date: str
+    PSA: float
+    imaging: bool
+    biopsy: str
+    actions: list [str] = []
+
+class SimilarPatientsResponse (BaseModel):
+    similar_patients: list [SimilarPatient] = []
