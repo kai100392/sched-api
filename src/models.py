a698551d485263ec29c89941776182a3550bb3bd
Lin, Yaxiong <Lin.Yaxiong@mayo.edu>
2024-11-18 22:01:37 +0000

    Updated models.py

--- src/models.py
+++ src/models.py
@@ -15,15 +15,25 @@ class SimilarPatient (BaseModel):
     biopsy: str
     actions: list [str] = []
 
-
+# this class must match analysis-svc's
 class PatientRequest (BaseModel):
+    APPT_TIME: str | None = None
+    CE_data: bool | False = False
+    PROSTATE_CANCER_ENC_APPT_DEP_SPECIALTY_FIRST: str | None = None
+    PROSTATE_CANCER_ENC_APPT_PRC_ID_FIRST: str | None = None
+    PROSTATE_CANCER_ENC_VISIT_NAME_FIRST: str | None = None
+    PROSTATE_CANCER_REFERRAL_CLASS: str | None = None
+    PROSTATE_CANCER_REQUEST_METHOD_C_FIRST: str | None = None
     PROSTATE_CANCER_VISIT_AGE_FIRST: float | None = None
+    all_orders: str | None = None
     biopsy_1: str | None = None
     biopsy_1_days: float | None = None
     biopsy_1_abnormal: bool | None = None
     biopsy_2: str | None = None
     biopsy_2_days: float| None = None
     biopsy_2_abnormal: bool | None = None
+    icd_10_1: str | None = None
+    icd_10_2: str | None = None
     imaging_1: str | None = None
     imaging_1_days: float | None = None
     imaging_1_abnormal: bool | None = None
@@ -54,6 +64,22 @@ class PatientRequest (BaseModel):
     psa_4_unit: str | None = None
     psa_4_abnormal: bool | None = None
     psa_recent_increase_percent: float | None = None
+    biopsy_1_days_cat: str | None = None
+    biopsy_2_days_cat: str | None = None
+    imaging_1_days_cat: str | None = None
+    imaging_2_days_cat: str | None = None
+    imaging_3_days_cat: str | None = None
+    psa_1_days_cat: str | None = None
+    psa_2_days_cat: str | None = None
+    psa_3_days_cat: str | None = None
+    psa_4_days_cat: str | None = None
+    psa_1_value_cat: str | None = None
+    psa_2_value_cat: str | None = None
+    psa_3_value_cat: str | None = None
+    psa_4_value_cat: str | None = None
+    psa_recent_increase_percent_cat: str | None = None
+    PROSTATE_CANCER_VISIT_AGE_FIRST_cat: str | None = None
+
 
 class UserInfo (BaseModel):
     userEmail: str
